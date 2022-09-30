43fe0511d9ed110ea5611bf50c02eb767cea5e14
IB1148981 <103644657+IB1148981@users.noreply.github.com>
2022-10-01 00:53:41 +0500

    fix(after): errors in bufferline.rc.lua

--- after/plugin/bufferline.rc.lua
+++ after/plugin/bufferline.rc.lua
@@ -1,96 +1,37 @@
 local status, bufferline = pcall(require, "bufferline")
 if (not status) then return end
 
--- bufferline.setup({
---   options = {
---     mode = "tabs",
---     separator_style = 'slant',
---     always_show_bufferline = false,
---     show_buffer_close_icons = false,
---     show_close_icon = false,
---     color_icons = true
---   },
---   highlights = {
---     separator = {
---       fg = '#073642',
---       bg = '#002b36',
---     },
---     separator_selected = {
---       fg = '#073642',
---     },
---     background = {
---       fg = '#657b83',
---       bg = '#002b36'
---     },
---     buffer_selected = {
---       fg = '#fdf6e3',
---       bold = true,
---     },
---     fill = {
---       bg = '#073642'
---     }
---   },
--- })
+bufferline.setup({
+  options = {
+    mode = "tabs",
+    separator_style = 'thick',
+    always_show_bufferline = false,
+    show_buffer_close_icons = false,
+    show_close_icon = false,
+    color_icons = true
+  },
+  highlights = {
+    separator = {
+      fg = '#fdf6e3',
+      -- fg = '#073642',
+      -- bg = '#002b36',
+    },
+    separator_selected = {
+      -- fg = '#073642',
+    },
+    background = {
+      fg = '#657b83',
+      -- bg = '#002b36'
+    },
 
--- VSCODE
-require("bufferline").setup({
-    options = {
-        buffer_close_icon = "",
-        close_command = "Bdelete %d",
-        close_icon = "",
-        indicator_icon = " ",
-        left_trunc_marker = "",
-        modified_icon = "●",
-        offsets = { { filetype = "NvimTree", text = "EXPLORER", text_align = "center" } },
-        right_mouse_command = "Bdelete! %d",
-        right_trunc_marker = "",
-        show_close_icon = false,
-        show_tab_indicators = true,
+    fill = {
+      -- bg = '#073642'
     },
-    highlights = {
-        fill = {
-            guifg = { attribute = "fg", highlight = "Normal" },
-            guibg = { attribute = "bg", highlight = "StatusLineNC" },
-        },
-        background = {
-            guifg = { attribute = "fg", highlight = "Normal" },
-            guibg = { attribute = "bg", highlight = "StatusLine" },
-        },
-        buffer_visible = {
-            gui = "",
-            guifg = { attribute = "fg", highlight = "Normal" },
-            guibg = { attribute = "bg", highlight = "Normal" },
-        },
-        buffer_selected = {
-            gui = "",
-            guifg = { attribute = "fg", highlight = "Normal" },
-            guibg = { attribute = "bg", highlight = "Normal" },
-        },
-        separator = {
-            guifg = { attribute = "bg", highlight = "Normal" },
-            guibg = { attribute = "bg", highlight = "StatusLine" },
-        },
-        separator_selected = {
-            guifg = { attribute = "fg", highlight = "Special" },
-            guibg = { attribute = "bg", highlight = "Normal" },
-        },
-        separator_visible = {
-            guifg = { attribute = "fg", highlight = "Normal" },
-            guibg = { attribute = "bg", highlight = "StatusLineNC" },
-        },
-        close_button = {
-            guifg = { attribute = "fg", highlight = "Normal" },
-            guibg = { attribute = "bg", highlight = "StatusLine" },
-        },
-        close_button_selected = {
-            guifg = { attribute = "fg", highlight = "normal" },
-            guibg = { attribute = "bg", highlight = "normal" },
-        },
-        close_button_visible = {
-            guifg = { attribute = "fg", highlight = "normal" },
-            guibg = { attribute = "bg", highlight = "normal" },
-        },
+    buffer_selected = {
+      fg = '#fdf6e3',
+      bold = true,
     },
+  },
 })
 
 vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
